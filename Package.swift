0eb71d348603f942b5f7382d91dcc3f5f2783da1
Muukii <muukii.app@gmail.com>
2019-10-17 05:30:06 +0900

    :evergreen_tree: Update

--- Package.swift
+++ Package.swift
@@ -4,20 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftJSONSyntax",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50100.0")),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "SwiftJSONSyntax",
-            dependencies: ["SwiftSyntax"]),
-        .testTarget(
-            name: "SwiftJSONSyntaxTests",
-            dependencies: ["SwiftJSONSyntax"]),
-    ]
+  name: "SwiftJSONSyntax",
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    // .package(url: /* package url */, from: "1.0.0"),
+    .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50100.0")),
+    .package(url: "https://github.com/apple/swift-package-manager.git", from: "0.3.0")
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+    .target(
+      name: "SwiftJSONSyntax",
+      dependencies: ["SwiftSyntax", "SPMUtility"]),
+    .testTarget(
+      name: "SwiftJSONSyntaxTests",
+      dependencies: ["SwiftJSONSyntax"]),
+  ]
 )
